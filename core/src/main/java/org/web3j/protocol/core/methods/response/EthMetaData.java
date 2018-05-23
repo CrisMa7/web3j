57386537a1272b87c52977a3d9587c1f4eb110c2
duanyytop <duanyytop@gmail.com>
2018-05-23 14:49:32 +0800

    update metadata class

--- core/src/main/java/org/web3j/protocol/core/methods/response/EthMetaData.java
+++ core/src/main/java/org/web3j/protocol/core/methods/response/EthMetaData.java
@@ -4,19 +4,25 @@ import org.web3j.abi.datatypes.Address;
 import org.web3j.protocol.core.Response;
 
 
-public class EthMetaData extends Response<EthMetaData> {
+public class EthMetaData extends Response<EthMetaData.EthMetaDataResult> {
 
-    public int chainId;
-    public String chainName;
-    public String operator;    //运营方
-    public String website;     //网站
-    public String genesisTimestamp;
-    public String basicToken;
-    public Address[] validators;
-    public int blockInterval;
-
-    public EthMetaData getEthMetaData() {
+    public EthMetaDataResult getEthMetaDataResult() {
         return getResult();
+    }
+
+    public boolean isEmpty() {
+        return getResult() == null;
+    }
+
+    public static class EthMetaDataResult {
+        public int chainId;
+        public String chainName;
+        public String operator;    //运营方
+        public String website;     //网站
+        public String genesisTimestamp;
+        public String basicToken;
+        public Address[] validators;
+        public int blockInterval;
     }
 
 }
